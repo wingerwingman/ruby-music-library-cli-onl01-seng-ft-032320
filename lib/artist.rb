d6b73025e85394257acabf8ba462fc7d0e9a673d
Justin Winger <justinwinger4@gmail.com>
2020-04-10 13:44:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,12 +4,20 @@ class Artist
   
   def initialize(name)
     @name = name 
-    @@all << self 
+    
   end
   
   def self.all
     @@all 
   end 
+  
+  def self.destroy_all
+    self.all.clear
+  end
+  
+  def save 
+    @@all << self
+  end
   
   
 end
